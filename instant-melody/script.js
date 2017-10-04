c0612ac61792885a3027da4149ebb60a5e68f1e9
mejiasparker <mejiasparker@gmail.com>
2017-10-03 22:49:30 -0400

    Update script.js

--- instant-melody/script.js
+++ instant-melody/script.js
@@ -1,6 +1,67 @@
-function setup() {
+staffobject = {
 
-noCanvas();
+  "cc": [329.63, 104],
+  "cd": [392, 84],
+  "dt": [440, 74],
+  "ex": [493.88, 64],
+  "fw": [587.33, 44],
+  "in": [329.63, 104],
+  "jj": [392, 84],
+  "jjr": [440, 74],
+  "jjs": [493.88, 64],
+  "ls": [587.33, 44],
+  "md": [329.63, 104],
+  "nn": [392, 84],
+  "nns": [440, 74],
+  "nnp": [493.88, 64],
+  "nnps": [587.33, 44],
+  "pdt": [329.63, 104],
+  "pos": [392, 84],
+  "prp": [440, 74],
+  "prp$": [493.88, 64],
+  "rb": [587.33, 44],
+  "rbr": [329.63, 104],
+  "rbs": [392, 84],
+  "rp": [440, 74],
+  "sym": [493.88, 64],
+  "to": [587.33, 44],
+  "uh": [329.63, 104],
+  "vb": [392, 84],
+  "vbd": [440, 74],
+  "vbg": [493.88, 64],
+  "vbn": [587.33, 44],
+  "vbp": [329.63, 104],
+  "vbz": [392, 84],
+  "wdt": [440, 74],
+  "wp": [493.88, 64],
+  "wp$": [587.33, 44],
+  "wrb": [329.63, 104]
+  
+};
+
+function setup() {
+  
+  let div = select('#results');
+  
+  canvas = createCanvas(windowWidth,120);
+  canvas.parent(div);
+  canvas.style('z-index','-1');
+  
+  
+  line(0,20,windowWidth,20);
+  line(0,40,windowWidth,40);
+  line(0,60,windowWidth,60);
+  line(0,80,windowWidth,80);
+  line(0,100,windowWidth,100);
+  
+//   fill(0, 102, 153);
+  
+//   text("E", 0, 104);
+//   text("G", 0, 84);
+//   text("A", 0, 74);
+//   text("B", 0, 64);
+//   text("D", 0, 44);
+  
 
 // select text input field
 input = select('#sentence');
@@ -14,27 +75,43 @@ clearbutton.mousePressed(clearP);
   function clearP(){
 
     let textresult = select('#results');
-    textresult.style('border-style', 'hidden');
+    // textresult.style('border-style', 'hidden');
     textresult.html('');
+    canvas.parent(textresult);
+  	canvas.style('z-index','-1');
 
   }
 
   function createSpans(){
+    
     let string = input.value();
-    string = string.replace(/[.,:;!@#$%&*()\n]+/, '');
+    string = string.replace(/[.,:;!'@"#$%&*()\n?]+/g, '');
     let wordarray = string.split(' ');
 
-    //console.log(wordarray);
-
-    let output = '';
     let textresult = select('#results');
     // textresult.class('text');
     // textresult.style('border-style', 'solid');
+    
+    let spanwidths = [];
+    let x = 0;
 
     for (let i=0; i < wordarray.length; i++){
+      let rs = new RiString(wordarray[i]);
+      let partofspeech = rs.pos();
+      
+      
       let span = createSpan(wordarray[i]);
+      
+      spanwidths.push(span.width);
       span.parent(textresult);
-      createSpan(' ').parent(textresult);
+
+      if (i == 0){
+        span.position(40, (staffobject[partofspeech][1] + 85));
+          } 
+      else {
+        x = x + spanwidths[i-1];
+        span.position(i * 10 + x + 40, (staffobject[partofspeech][1] + 85));
+      }
 
       span.mouseOver(highlight);
       span.mouseOut(unhighlight);
@@ -42,83 +119,6 @@ clearbutton.mousePressed(clearP);
 
     }
   }
-
-  
-posobject = {
-  "cd": 277.18,
-  "dt": 293.66,
-  "ex": 311.13,
-  "fw": 329.63,
-  "in": 349.23,
-  "jj": 369.99,
-  "jjr": 392,
-  "jjs": 415.3,
-  "ls": 440,
-  "md": 466.16,
-  "nn": 494,
-  "nns": 523.25,
-  "nnp": 554.37,
-  "nnps": 587.33,
-  "pdt": 622.25,
-  "pos": 659.25,
-  "prp": 698.46,
-  "prp$": 739.99,
-  "rb": 783.99,
-  "rbr": 830.61,
-  "rbs": 880,
-  "rp": 932.33,
-  "sym": 987.77,
-  "to": 1046.5,
-  "uh": 1108.73,
-  "vb": 1174.66,
-  "vbd": 1244.51,
-  "vbg": 1318.51,
-  "vbn": 1396.91,
-  "vbp": 1479.98,
-  "vbz": 1567.98,
-  "wdt": 1661.22,
-  "wp": 1760,
-  "wp$": 1864.66,
-  "wrb": 1975.53
-};
-  
-pentaobject =  {
-  "cd": 246.94,
-  "dt": 293.66,
-  "ex": 329.63,
-  "fw": 392,
-  "in": 440,
-  "jj": 493.88,
-  "jjr": 587.33,
-  "jjs": 659.25,
-  "ls": 783.99,
-  "md": 880,
-  "nn": 987.77,
-  "nns": 1174.66,
-  "nnp": 1318.51,
-  "nnps": 1567.98,
-  "pdt": 1760,
-  "pos": 1975.53,
-  "prp": 2349.32,
-  "prp$": 2637.02,
-  "rb": 2349.32,
-  "rbr": 1975.53,
-  "rbs": 1760,
-  "rp": 1567.98,
-  "sym": 1318.51,
-  "to": 1174.66,
-  "uh": 987.77,
-  "vb": 880,
-  "vbd": 783.99,
-  "vbg": 659.25,
-  "vbn": 587.33,
-  "vbp": 493.88,
-  "vbz": 440,
-  "wdt": 392,
-  "wp": 329.63,
-  "wp$": 293.66,
-  "wrb": 246.94
-};
   
 context = new (window.AudioContext || window.webkitAudioContext)();
   
@@ -131,7 +131,7 @@ context = new (window.AudioContext || window.webkitAudioContext)();
 
     let osc = context.createOscillator(); // instantiate an oscillator
     osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
-    osc.frequency.value = pentaobject[partofspeech]; // Hz
+    osc.frequency.value = staffobject[partofspeech][0]; // Hz
     osc.connect(context.destination); // connect it to the destination
     osc.start(); // start the oscillator
     osc.stop(context.currentTime + .5); // stop 2 seconds after the current time
